fbb9e460ba3937b6242dc88d6d2c34efcc5be3fb
imousa <isra.mousa.95@hotmail.com>
2018-10-25 13:56:46 +0300

    connect to database

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
+
+const app = express();
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago')
+mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
 app.use('/api',require('./routes/api'));
+app.use((err, req, res, next) => {
+    res.send({error:err.message})
+})
 
 app.listen(process.env.port || 4000, () =>{
     console.log("listening to request")
