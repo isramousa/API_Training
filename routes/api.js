c3f61917b1b61987707a92dff15eeae7fccb0869
imousa <isra.mousa.95@hotmail.com>
2018-10-25 13:57:56 +0300

    error handling

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,29 @@
 const express = require('express');
 
 const router = express.Router();
-
+const Ninja = require('../models/ninja')
 
 //get a list of ninjas from db
-router.get('/ninjas',(req, res)=>{
+router.get('/ninjas',(req, res, next)=>{
     res.send({type:'GET'})
 })
 
 //add a new ninja to the db
-router.post('/ninjas',(req, res)=>{
-    console.log(req.body);
-    res.send({
-        type:'POST',
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
+router.post('/ninjas',(req, res, next)=>{
+    Ninja.create(req.body)
+    .then((result)=>{
+        res.send(result)
     })
+    .catch(next)
 })
 
 //update a ninja in the db
-router.put('/ninjas/:id',(req, res)=>{
+router.put('/ninjas/:id',(req, res, next)=>{
     res.send({type:'PUT'})
 })
 
 //delete a ninja from the db 
-router.delete('/ninjas/:id',(req, res)=>{
+router.delete('/ninjas/:id',(req, res, next)=>{
     res.send({type:'DELETE'})
 })
 
